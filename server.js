1d2d02129d03597224eb68d25e2229465ebc0c57
Viktor Lishchuk <s20817@pjwstk.edu.pl>
2022-01-24 13:43:56 +0100

    dev 7
    -Loose screen was added
    -Win screen was added
    -Music was added
    -Game logic was adjusted

--- server.js
+++ server.js
@@ -4,10 +4,24 @@ const app = express();
 
 
 app.set('view engine', 'ejs');
-app.get('/', (req, res) => 
+app.get('/game', (req, res) => 
 {
     res.sendFile('./game/index.html', { root: __dirname });
 });
 
-app.listen(3000);
+app.get('/win', (req, res) => 
+{
+    res.sendFile('./game/winScreen.html', { root: __dirname });
+});
 
+app.get('/loose', (req, res) => 
+{
+    res.sendFile('./game/looseScreen.html', { root: __dirname });
+});
+
+app.get('/', (req, res) => 
+{
+    res.sendFile('./game/menu.html', { root: __dirname });
+});
+
+app.listen(3000);
