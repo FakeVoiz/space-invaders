26ccf001800f3a7cf2d976e74942a3b3bfac499c
Viktor Lishchuk <91957137+FakeVoiz@users.noreply.github.com>
2022-01-24 21:06:52 +0100

    dev 9
    -Best time was added

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 'use strict'
 const express = require(`express`);
 const app = express();
+const fs = require('fs');
+const parser = require('body-parser');
 
+var bestTime = Number.MAX_VALUE;
+var lastTime;
 
 app.set('view engine', 'ejs');
+app.use(parser.json());
 app.get('/game', (req, res) => 
 {
-    res.sendFile('./game/index.html', { root: __dirname });
+    res.sendFile('./game/game.html', { root: __dirname });
 });
 
 app.get('/win', (req, res) => 
 {
-    res.sendFile('./game/winScreen.html', { root: __dirname });
+    fs.readFile('./game/winScreen.html', 'utf8' , (err, data) => 
+    {
+        if (err)
+        {
+          console.error(err);
+          return;
+        }
+
+        res.send(data.replace('%bestTime%', `${bestTime}`).replace('%yourTime%', `${lastTime}`));
+    });      
 });
 
 app.get('/loose', (req, res) => 
@@ -22,6 +36,13 @@ app.get('/loose', (req, res) =>
 app.get('/', (req, res) => 
 {
     res.sendFile('./game/menu.html', { root: __dirname });
+});
+
+app.post('/data', (req, res) =>
+{
+    var timeTakenToClearLevel = req.body.value;
+    lastTime = timeTakenToClearLevel;
+    bestTime = Math.min(bestTime, lastTime);
 });
 
 app.listen(3000);
